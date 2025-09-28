d767ecba575f7fc48dcd583ea99b774cb9a86540
Liliia <liliyamalishnya@gmail.com>
2025-09-28 13:02:16 +0300

    update page.tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,7 +1,21 @@
-import NoteDetails from "./NoteDetails";
 import { NoteDetailsPageProps } from "../../../types/params";
+import { HydrationBoundary, dehydrate, QueryClient } from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api";
+import NoteDetailsClient from "./NoteDetails.client";
 
 
-export default function NoteDetailsPage({ params }: NoteDetailsPageProps) {
-    return <NoteDetails params={params} />;
+export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) {
+  const { id } = await params;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
+  );
 }
